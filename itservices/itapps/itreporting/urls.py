21a3f2776fd6d34b1ff20b1a62c43b542af2e6c5
rasilaratnakaran <rasilaratnakaran@gmail.com>
2023-02-14 13:37:25 +0000

    My first project on Cloud Class

---
+++ itservices/itapps/itreporting/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns =[
+
+
+path('', views.home, name='itreporting-home'),
+
+path('about/', views.about, name='itreporting-about'),
+path('contact/', views.about, name='itreporting-contact'),
+]
